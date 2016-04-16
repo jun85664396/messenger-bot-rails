dfb6fb7d47724181b059042911c16170b8a3d5b3
JunSangPil <jun85664396@gmail.com>
2016-04-16 22:22:43 +0900

    hotfix typo

--- lib/facebook/bot/station.rb
+++ lib/facebook/bot/station.rb
@@ -2,7 +2,7 @@ module Facebook
   module Bot
     class Space::StationController < ::ApplicationController
       def validation
-        if params["hub.verify_token"] === Facebook::Bot::Config.verify_token
+        if params["hub.verify_token"] === Facebook::Bot::Config.validation_token
           return render json: params["hub.challenge"]
         end
         render body: "Error, wrong validation token"
